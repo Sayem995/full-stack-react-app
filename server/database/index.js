3310a1d59827d1bbfbbef4a2c5b5fc86b867320a
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-02 19:29:13 +0000

    added constructor, query and end methods to database class

--- server/database/index.js
+++ server/database/index.js
@@ -10,13 +10,34 @@ class Database {
       connectionString: CONNECTION_STRING,
       ssl: SSL
     });
+
+    this._pool.on('error', (err, client) => {
+      console.error('Unexpected error on idle PostgreSQL client.', err);
+      process.exit(-1);
+    });
+
   }
+
   query () {
+    this._pool.connect((err, client, done) => {
+      if(err) throw err;
+      const params = args.length === 2 ? args[0] : [];
+      const callback = args.length === 1 ? args[0] : args[1];
+
+      client.query(query, params, (err, res) => {
+        done();
+        if(err){
+          console.log(err.stack);
+          return callback({ error: 'Database error.' }, null);
+        }
+        callback({}, res.rows);
+      });
+    });
 
   }
 
   end () {
-
+    this._pool.end();
   }
 }
 
